dd39011758180523f778fa036a9b44ee6b0598bc
moon-xu37 <326167868@qq.com>
2021-12-11 23:41:57 +0800

    write the multiplier functions

--- src/numeric.py
+++ src/numeric.py
@@ -23,31 +23,42 @@ class Multiplier():
 
     @staticmethod
     def statProcessor(data: dict, *buffs):
-        pass
+        dependency = data['dependency']
+        return data[f'{dependency}']
 
     @staticmethod
     def scalarProcessor(data: dict, *buffs):
-        pass
+        return data['scalar']
 
     @staticmethod
     def damageBonusProcessor(data: dict, *buffs):
-        pass
+        dependency = data['dependency']
+        bonus = sum([b for b in buffs])+data[f'{dependency}_bonus']
+        return 1+bonus
 
     @staticmethod
     def critDamageProcessor(data: dict, *buffs):
-        pass
+        return 1+data['crit_damage']*data['crit_rate']
 
     @staticmethod
     def reactionProcessor(data: dict, *buffs):
-        pass
+        return 1+(2.78*data['EM'])/(1400+data['EM'])+data['react_bonus']
 
     @staticmethod
     def resistanceProcessor(data: dict, *buffs):
-        pass
+        elem = data.get('elem_type')
+        reduce = data['monster'][f'{elem}_res'] - \
+            data.get(f'{elem}_res_reduce', 0)
+        if reduce > 75:
+            return 1/(1+0.04*reduce)
+        elif 0 <= reduce <= 75:
+            return 1-0.01*reduce
+        else:
+            return 1-0.005*reduce
 
     @staticmethod
     def defenceProcessor(data: dict, *buffs):
-        pass
+        return (data['level']+100)/(data['level']+100+(1-data['def_ignore'])(1-data['def_reduce'])(data['monster']['level']+100))
 
     __type_map = \
         {
@@ -62,10 +73,17 @@ class Multiplier():
 
     def __init__(self, type: str) -> None:
         self.func = self.__type_map.get(type, None)
+        self.data = dict()
+        self.buffs = None
+        self.value = 1.0
 
-    def merge(self, ID: str, *multipliers):
-        '''ID should not be repeated'''
-        pass
+    def update(self, data, *buffs):
+        self.data = data
+        self.buffs = buffs
+        self.value = self.func(data, buffs)
+
+    def __float__(self) -> float:
+        return self.value
 
 
 class Damage():
@@ -77,20 +95,25 @@ class Damage():
 
 
 class AmplifyingDamage(Damage):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
 
 
 class TransformativeDamage(Damage):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
 
 
 class blankDamage(Damage):
-    pass
+    def __init__(self) -> None:
+        super().__init__()
 
 
 class Heal():
-    pass
+    def __init__(self) -> None:
+        pass
 
 
 class Shield():
-    pass
+    def __init__(self) -> None:
+        pass
